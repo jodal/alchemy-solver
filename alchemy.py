9ac724dac2334b121dc4e694e2d2779e95321569
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-01-01 18:41:01 +0100

    Split and reorganize methods

--- alchemy.py
+++ alchemy.py
@@ -9,18 +9,18 @@ class Solution(object):
         self.graph = pygraphviz.AGraph(directed=True)
         self.id_to_name = {}
 
+    @property
+    def elements(self):
+        return self.id_to_name.values()
+
     def add_elements(self, filename):
-        with open(filename) as filehandle:
-            xml = filehandle.read()
-        for entry in fromstring(xml).xpath('//entry'):
+        for entry in self._get_xml(filename).xpath('//entry'):
             self.graph.add_node(entry.text,
                 color='lightblue2', style='filled')
             self.id_to_name[entry.attrib['id']] = entry.text
 
     def add_relations(self, filename):
-        with open(filename) as filehandle:
-            xml = filehandle.read()
-        for entry in fromstring(xml).xpath('//entry'):
+        for entry in self._get_xml(filename).xpath('//entry'):
             child = self.id_to_name[entry.attrib['id']]
             for parents in entry.findall('parents'):
                 parents = [self.id_to_name[p.attrib['id']] for p in parents]
@@ -29,25 +29,36 @@ class Solution(object):
                     self.graph.add_edge(parent, combination)
                 self.graph.add_edge(combination, child)
 
-    @property
-    def elements(self):
-        return self.id_to_name.values()
+    def _get_xml(self, filename):
+        with open(filename) as filehandle:
+            return fromstring(filehandle.read())
 
-    def save_graph(self, element=None):
-        if element is None:
-            filename = 'solutions/FULL.png'
-            graph = self.graph
+    def process_command(self, command):
+        if command == '--full':
+            self.save_full_graph()
+        elif command == '--all':
+            self.save_full_graph()
+            for element in self.elements:
+                self.save_element_graph(element)
         else:
-            filename = 'solutions/%s.png' % (
-                element.replace(' ', '_').replace('!', ''))
-            graph = self.get_predecessor_graph(element)
-            graph.get_node(element).attr['color'] = 'green'
+            self.save_element_graph(command)
+
+    def save_full_graph(self):
+        self._save_graph(self.graph, 'solutions/FULL.png')
+
+    def save_element_graph(self, element):
+        filename = 'solutions/%s.png' % (
+            element.replace(' ', '_').replace('!', ''))
+        graph = self._get_predecessor_graph(element)
+        graph.get_node(element).attr['color'] = 'green'
+        self._save_graph(graph, filename)
+        graph.get_node(element).attr['color'] = 'lightblue2'
+
+    def _save_graph(self, graph, filename):
         graph.draw(filename, prog='dot')
         print '%s generated' % filename
-        if element is not None:
-            graph.get_node(element).attr['color'] = 'lightblue2'
 
-    def get_predecessor_graph(self, element):
+    def _get_predecessor_graph(self, element):
         try:
             child = self.graph.get_node(element)
         except KeyError:
@@ -61,16 +72,6 @@ class Solution(object):
             nodes.append(child)
             nodes_to_visit.extend(self.graph.predecessors(child))
         return self.graph.subgraph(nbunch=nodes)
-
-    def process_command(self, command):
-        if command == '--full':
-            self.save_graph()
-        elif command == '--all':
-            self.save_graph()
-            for element in self.elements:
-                self.save_graph(element=element)
-        else:
-            self.save_graph(element=command)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
